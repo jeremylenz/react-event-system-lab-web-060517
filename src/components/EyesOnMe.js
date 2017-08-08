f54139a01011bc62a5d17a1c7075832299bc6190
jeremylenz <jeremylenz@gmail.com>
2017-08-08 10:09:19 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  render () {
+    return (
+      <button onFocus={this.printGood} onBlur={this.printEyes} />
+    )
+  }
+
+  printGood() {
+    console.log('Good!')
+  }
+
+  printEyes() {
+    console.log('Hey! Eyes on me!')
+  }
+
+
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+  render () {
+    return (
+      <input type="password" onKeyUp={this.consoleDisplay}/>
+    )
+  }
+
+  consoleDisplay() {
+    console.log('Entering password...')
+  }
+
+}
+
+export default Keypad
